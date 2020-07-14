8f55e1b7bd8fcbc89948422bcd507b3575110b5c
lianmafutra <lianmafutra@gmail.com>
2020-07-14 09:01:27 +0700

    Update MoveActivity.java

--- intent/MoveActivity.java
+++ intent/MoveActivity.java
@@ -1,1 +1,12 @@
- startActivity(new Intent(getContext(), LaporList.class));
+ startActivity(new Intent(getContext(), LaporList.class));  
+// 1. SIMPLE 
+  startActivity(new Intent(getContext(), LaporList.class));
+  
+
+// 2. WITH INTENT DATA
+  Intent data = new Intent(this, SecondActivity.class);
+        data.putExtra("key", "value");
+        ctx.startActivity(data); 
+
+//get data intent
+getIntent().getStringExtra("key");
